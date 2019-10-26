254f36b43d78436931bc136b35997103665b6825
Viktor Dick <viktor.dick@perfact.de>
2019-10-26 20:38:17 +0200

    Rename name of error handler and pass a string as error_type

--- Products/PerFactErrors/errors.py
+++ Products/PerFactErrors/errors.py
@@ -8,21 +8,25 @@ logger = logging.getLogger(__name__)
 
 
 @zope.component.adapter(ZPublisher.interfaces.IPubFailure)
-def standard_error_message(event):
-    """Render error message after an aborted transaction."""
-    # The transaction in which the exception was raised gets aborted just
-    # before IPubFailure, so we can safely commit a new one during our error
-    # handling procedure. This is also the reason why we cannot use a logging
-    # view directly.
+def afterfail_error_message(event):
+    """
+    Render error message after an aborted transaction.
+    The transaction in which the exception was raised gets aborted just before
+    IPubFailure, so we can safely commit a new one during our error handling
+    procedure. This is also the reason why we cannot use a logging view
+    directly.
+    """
     req = event.request
     context = req['PARENTS'][0]
-    render = getattr(context, 'perfact_error_handler_', None)
+    render = getattr(context, 'afterfail_error_message_', None)
     if render is None:
         return
     try:
         error_type, error_value, error_tb = event.exc_info
+        # Inside a method inside Zope, we can handle a string better, so we
+        # pass the __name__ as error_type
         body = render(
-            error_type=error_type,
+            error_type=error_type.__name__,
             error_value=error_value,
             error_tb=error_tb,
         )
